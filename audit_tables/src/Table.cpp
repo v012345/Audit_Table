d4cfa24878d84fd3bdf3d4e7fcbde5b97c46e054
v138468 <v138468@gmail.com>
2022-10-16 15:07:57 +0800

    add audit_tables

---
+++ audit_tables/src/Table.cpp
@@ -0,0 +1,149 @@
+#include "table.h"
+Table::Table(std::string path)
+{
+    this->name = path;
+    this->table.open(path);
+    this->sheet = table.workbook().worksheet(table.workbook().worksheetNames()[0]);
+
+    std::map<std::string, std::uint32_t> head;
+    OpenXLSX::XLRowIterator row = sheet.rows().begin();
+    int index = 0;
+    for (auto &value : std::vector<OpenXLSX::XLCellValue>(row->values()))
+    {
+        head.insert(std::make_pair(value.get<std::string>(), index));
+        index++;
+    }
+    this->head = head;
+}
+Table::Table(std::string path, std::string name)
+{
+    this->name = name;
+    this->table.open(path);
+    this->sheet = table.workbook().worksheet(table.workbook().worksheetNames()[0]);
+
+    std::map<std::string, std::uint32_t> head;
+    OpenXLSX::XLRowIterator row = sheet.rows().begin();
+    int index = 0;
+    for (auto &value : std::vector<OpenXLSX::XLCellValue>(row->values()))
+    {
+        head.insert(std::make_pair(value.get<std::string>(), index));
+        index++;
+    }
+    this->head = head;
+}
+std::string Table::getName()
+{
+    return this->name;
+};
+void Table::setPrimaryKey(std::string primary_key)
+{
+    std::set<std::int32_t> primary_keys;
+    for (OpenXLSX::XLCellValue &v : this->getColumn(primary_key))
+    {
+        if (v.type() == OpenXLSX::XLValueType::Integer)
+        {
+            primary_keys.insert(v.get<std::int32_t>());
+        }
+        else if (v.type() == OpenXLSX::XLValueType::String)
+        {
+            primary_keys.insert(stoi(v.get<std::string>()));
+        }
+        else
+        {
+            break;
+        }
+    }
+    this->primary_key = primary_keys;
+}
+void Table::insertForeignKeys(std::string foreign_key)
+{
+    if (this->foreign_keys.find(foreign_key) != this->foreign_keys.end())
+    {
+        return;
+    }
+    std::set<std::int32_t> foreign_keys;
+    for (OpenXLSX::XLCellValue &v : this->getColumn(foreign_key))
+    {
+        if (v.type() == OpenXLSX::XLValueType::Integer)
+        {
+            // std::cout << m.get<int>() << std::endl;
+            foreign_keys.insert(v.get<std::int32_t>());
+        }
+        else if (v.type() == OpenXLSX::XLValueType::String)
+        {
+            std::stringstream test(v.get<std::string>().c_str());
+            std::string segment;
+            std::vector<std::string> seglist;
+
+            while (std::getline(test, segment, '|'))
+            {
+                try
+                {
+                    foreign_keys.insert(stoi(segment));
+                }
+                catch (const std::exception &e)
+                {
+                    if ( this->name != "item_artifact_star")
+                    {
+                       std::cout << this->name << " column(" << foreign_key << ") has a value " << segment << " which can convert to int" << std::endl;
+                    }
+                    // std::cerr << e.what() << '\n';
+                }
+            }
+        }
+    }
+
+    this->foreign_keys.insert(std::make_pair(foreign_key, foreign_keys));
+}
+std::map<std::string, std::set<std::int32_t>> Table::getForeignKeys()
+{
+    return this->foreign_keys;
+}
+std::map<std::string, std::uint32_t> Table::getHead()
+{
+    return this->head;
+}
+std::set<std::int32_t> Table::getForeignKey(std::string foreign_key)
+{
+    return this->foreign_keys.at(foreign_key);
+}
+
+std::set<std::int32_t> Table::getPrimaryKey()
+{
+    return this->primary_key;
+}
+
+std::vector<OpenXLSX::XLCellValue> Table::getColumn(std::string column_name)
+{
+    if (this->data.find(column_name) == this->data.end())
+    {
+        OpenXLSX::XLRowIterator row = sheet.rows().begin();
+        row++;
+        std::vector<OpenXLSX::XLCellValue> column_data;
+        ;
+        while (row != this->sheet.rows().end())
+        {
+            try
+            {
+                OpenXLSX::XLCellValue m = std::vector<OpenXLSX::XLCellValue>(row->values()).at(this->head.at(column_name));
+                if (m.type() == OpenXLSX::XLValueType::Empty)
+                    break;
+                column_data.push_back(m);
+
+                row++;
+            }
+            catch (const std::exception &e)
+            {
+                // std::cout << "does " << this->name << " have " << this->sheet.rowCount() << " rows? I think it has some blank rows" << std::endl;
+                break;
+            }
+        }
+        // std::cout << this->name << " have " << column_data.size() << " rows" << std::endl;
+        this->data.insert(std::make_pair(column_name, column_data));
+    }
+    return this->data.at(column_name);
+}
+Table::~Table()
+{
+    this->table.close();
+}
